9bc915e946eb5ad80786911269a96f10ffd2ca75
mikaylecollins <mkcollins327@gmail.com>
2022-01-20 21:20:29 -0500

    screen

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let canvas = document.getElementById("gameScreen");
 //creating a rectangle and filling it
 let ctx = canvas.getContext('2d');
+ctx.fillRect(50,50,100,100);
 ctx.fillStyle = '#f00';
-ctx.fillRect(20,20,100,100);
+
 //clearing the canvas - good for gameover or moving
 //ctx.clearRect(0,0,800,600);
 
